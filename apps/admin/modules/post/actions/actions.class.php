2a05992c3985d1930d7e5fcf3a02c0deb0f1c2f5
Tristan Rivoallan <tristan@rivoallan.net>
2010-03-14 19:32:40 +0100

    [admin] Generated admin module using "doctrine:generate-admin".

---
+++ apps/admin/modules/post/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/postGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/postGeneratorHelper.class.php';
+
+/**
+ * post actions.
+ *
+ * @package    musique-approximative
+ * @subpackage post
+ * @author     Tristan Rivoallan <tristan@rivoallan.net>
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class postActions extends autoPostActions
+{
+}
---
+++ lib/form/doctrine/PostForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Post form.
+ *
+ * @package    musique-approximative
+ * @subpackage form
+ * @author     Tristan Rivoallan <tristan@rivoallan.net>
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class PostForm extends BasePostForm
+{
+  public function configure()
+  {
+  }
+}
