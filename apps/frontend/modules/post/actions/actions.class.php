a9bf4a8c48ff70db857c3743916f7d8854285801
Tristan Rivoallan <tristan@rivoallan.net>
2010-03-14 19:37:51 +0100

    [frontend] Generated post module using "generate:module"

---
+++ apps/frontend/modules/post/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * post actions.
+ *
+ * @package    musique-approximative
+ * @subpackage post
+ * @author     Tristan Rivoallan <tristan@rivoallan.net>
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class postActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
