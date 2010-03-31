67ea0b02b1cd1e32511101499818b3bd929fbf2c
Tristan Rivoallan <tristan@rivoallan.net>
2010-04-01 01:15:54 +0200

    [frontend] Integrated google analytic marker.

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -53,5 +53,14 @@
       <p>Ce projet est hébergé par <a href="http://www.pastis-hosting.net">Pastis Hosting</a>.</p>
       <p>Contact : bertier at musiqueapproximative point net</p>
     </div>
+    <script type="text/javascript">
+    var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+    document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+    </script>
+    <script type="text/javascript">
+    try {
+      var pageTracker = _gat._getTracker("UA-4958604-1");
+      pageTracker._trackPageview();
+    } catch(err) {}</script>
   </body>
 </html>
