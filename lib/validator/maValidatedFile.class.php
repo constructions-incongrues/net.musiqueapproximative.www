a60c49366d621f749b397e181ee653e5fb88adf5
Tristan Rivoallan <tristan@rivoallan.net>
2011-07-15 13:12:45 +0200

    [admin] Fixed issue when posting track with single quote in filename

--- lib/validator/maValidatedFile.class.php
+++ lib/validator/maValidatedFile.class.php
@@ -3,6 +3,7 @@ class maValidatedFile extends sfValidatedFile
 {
   public function generateFilename()
   {
-    return filter_var($this->getOriginalName(), FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
+  	$filename = str_replace("'", ' ', $this->getOriginalName());
+  	return filter_var($filename, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
   }  
 }
