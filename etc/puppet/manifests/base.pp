1dc0c1b08ecc868c0fbc72af81633cc2d1c5e451
Tristan Rivoallan <tristan@rivoallan.net>
2012-11-26 15:09:29 +0100

    VAGRANTIFIED !

--- etc/puppet/manifests/base.pp
+++ etc/puppet/manifests/base.pp
@@ -1,8 +1,8 @@
 class net::musiqueapproximative::www {
-  # TODO : make sure this is executed before packages installation
   exec { 'apt-get update':
     command => '/usr/bin/apt-get update'
   }
+  Exec["apt-get update"] -> Package <| |>
 
   exec { 'a2enmod rewrite':
     command => '/usr/sbin/a2enmod rewrite',
@@ -10,30 +10,43 @@ class net::musiqueapproximative::www {
     notify => Service['apache2']
   }
 
+  exec { 'a2dissite default':
+    command => '/usr/sbin/a2dissite default',
+    require => Package['apache2'],
+    notify => Service['apache2']
+  }
+
+  exec { 'mysql load net_musiqueapproximative_www dump': 
+    command => '/usr/bin/mysql -uroot < /vagrant/src/data/fixtures/net_musiqueapproximative_www.dump.sql',
+    require => Package['mysql-server']
+  }
+
   package { ["apache2", "libapache2-mod-php5", "php5-cli", "php5-mysqlnd", "php-apc", "vim-tiny", "mysql-server"]:
     ensure => present,
   }
 
-  service { ["apache2"]:
+  service { "apache2":
     ensure => running,
     require => Package["apache2"],
+    subscribe => File['/etc/apache2/envvars']
   }
 
-  file { '/etc/apache2/sites-enabled/000-default':
-    ensure => absent,
-    notify => Service['apache2']
+  service { ["mysql"]:
+    ensure => running,
+    require => Package["mysql-server"],
   }
 
   file { '/etc/apache2/sites-enabled/net_musiqueapproximative_www':
     ensure => present,
     source => "puppet:///vagrant_puppet_files/etc/apache2/sites-enabled/net_musiqueapproximative_www",
-    notify => Service['apache2']
+    notify => Service['apache2'],
+    require => Package["apache2"],
   }
 
   file { '/etc/apache2/envvars':
     ensure => present,
     source => "puppet:///vagrant_puppet_files/etc/apache2/envvars",
-    notify => Service['apache2']
+    require => Package["apache2"],
   }
 }
 
