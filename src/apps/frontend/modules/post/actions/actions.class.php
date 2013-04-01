e65abb28a7f5572d36e8f6f014dabf996113a564
Tristan Rivoallan <tristan@rivoallan.net>
2013-04-01 20:17:29 +0200

    [front] fixed contributor posts list

--- src/apps/frontend/modules/post/actions/actions.class.php
+++ src/apps/frontend/modules/post/actions/actions.class.php
@@ -111,10 +111,10 @@ class postActions extends sfActions
     }
     else
     {
-      $posts = Doctrine::getTable('Post')->getOnlinePosts($request->getParameter('contributor'));
-      if ($request->getParameter('contributor'))
+      $posts = Doctrine::getTable('Post')->getOnlinePosts($request->getParameter('c'));
+      if ($request->hasParameter('c'))
       {
-        $list_title = sprintf('%s a posté %d morceau(x) à ce jour', $request->getParameter('contributor'), count($posts));
+        $list_title = sprintf('%s a posté %d morceau(x) à ce jour', $request->getParameter('c'), count($posts));
       }
     }
 
