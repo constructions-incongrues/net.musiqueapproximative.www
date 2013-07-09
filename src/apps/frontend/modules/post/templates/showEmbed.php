992793d39fb99cc01c83756a9692f35300d4d057
Johan <bozoorak@gmail.com>
2013-07-09 21:24:05 +0200

    Css sur le embed !

--- src/apps/frontend/modules/post/templates/showEmbed.php
+++ src/apps/frontend/modules/post/templates/showEmbed.php
@@ -2,10 +2,17 @@
 <?php decorate_with(false) ?>
 <?php sfConfig::set('sf_web_debug', false) ?>
 <style>
+
+@import url(http://fonts.googleapis.com/css?family=Arvo);
+@import url(http://fonts.googleapis.com/css?family=Rambla);
+
 div.musiqueapproximative {width:500px;border:solid 1px black;background-color: #fff;}
+.musiqueapproximative h3 {font-family:'Rambla',  sans-serif;}
+.musiqueapproximative h4 {font-family:'Arvo',  sans-serif;font-size: 0.8em}
 .musiqueapproximative h3, .musiqueapproximative h4 {background-color:#000;color:#fff;padding:0.3em;margin:0;text-align: center;}
-.musiqueapproximative a {color:#fff;}
-.musiqueapproximative p {padding:0.3em;}
+.musiqueapproximative a {color:#fff;text-decoration: underline;}
+.musiqueapproximative p a {color:#555050;text-decoration: underline;}
+.musiqueapproximative p {padding:0.3em;font-family:'Arvo',  sans-serif;text-align: center;}
 </style>
 <div class="musiqueapproximative">
 	<h3><a title="Voir le morceau sur le site Musique Approximative" href="<?php echo url_for('@post_show?slug='.$post->slug) ?>" target="_blank"><?php echo $post->track_author ?> - <?php echo $post->track_title ?></a></h3>
