8c267657f18b6498d289f9a933f4b1583d348481
Tristan Rivoallan <tristan.rivoallan@aramisauto.com>
2015-01-28 21:59:31 +0100

    [admin] finally fix the & bug !

--- src/lib/validator/maValidatedFile.class.php
+++ src/lib/validator/maValidatedFile.class.php
@@ -5,6 +5,7 @@ class maValidatedFile extends sfValidatedFile
   {
   	$filename = str_replace("'", ' ', $this->getOriginalName());
   	$filename = str_replace("\\", ' ', $filename);
+  	$filename = str_replace('&', '-', $filename);
   	return filter_var($filename, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH);
-  }  
+  }
 }
