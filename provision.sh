400bb358ddfa58a745347411e378b66f1906e207
Tristan Rivoallan <tristan.rivoallan@aramisauto.com>
2015-02-11 11:50:53 +0100

    [provisioning] activation de mod_rewrite

--- provision.sh
+++ provision.sh
@@ -4,12 +4,14 @@
 apt-get -qq update
 
 # Configuration de la timezone
-echo "Europe/Paris" > /etc/timezone 
+echo "Europe/Paris" > /etc/timezone
 apt-get install -y tzdata
 dpkg-reconfigure -f noninteractive tzdata
 
 # Installation de Apache et PHP
 apt-get -y install libapache2-mod-php5 php5-cli
+a2enmod rewrite
+service apache2 restart
 
 # Installation de MySQL
 echo "mysql-server mysql-server/root_password password root" | debconf-set-selections
@@ -30,7 +32,7 @@ mysql --defaults-file=/etc/mysql/debian.cnf -e "create database net_musiqueappro
 mysql --defaults-file=/etc/mysql/debian.cnf net_musiqueapproximative_www < /vagrant/src/data/fixtures/net_musiqueapproximative_www.dump.sql
 
 # Configuration du projet
-apt-get install -y ant 
+apt-get install -y ant
 cd /vagrant
 ./composer.phar install --prefer-dist --no-progress
 ant configure build -Dprofile=vagrant
@@ -41,6 +43,6 @@ ln -sf /vagrant/src/web/* /var/www/
 rm -f /var/www/index.html
 
 # Informations
-echo 
+echo
 echo -e "Le site est disponible à l'adresse : http://musiqueapproximative.vagrant.dev/frontend_dev.php"
 echo -e "PhpMyAdmin est disponible à l'adresse : http://musiqueapproximative.vagrant.dev/phpmyadmin/ (root / root)"
