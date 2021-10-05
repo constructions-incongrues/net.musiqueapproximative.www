e0ca08322cc91cf346cb3525bc66a7cd55d45f6c
Tristan Rivoallan <tristan@rivoallan.net>
2021-10-05 18:37:43 +0200

    Fix check unicité

--- src/web/js/admin/behaviors.js
+++ src/web/js/admin/behaviors.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	
 	$('#post_is_unique').click(function() {
-		var url = 'http://www.google.com/#q='+encodeURIComponent('site:http://www.musiqueapproximative.net ' + $('#post_track_title').val());
+		var url = 'http://www.google.com/search?q='+encodeURIComponent('site:http://www.musiqueapproximative.net ' + $('#post_track_title').val());
 		window.open(url);
 	});
 	
