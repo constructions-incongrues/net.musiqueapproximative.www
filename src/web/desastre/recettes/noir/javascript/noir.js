ab3f46ada654d120f576464f1e222cf7ced9ddaf
nota2k <nelly.babillon@gmail.com>
2025-04-22 23:28:01 +0200

    feat(noir): ajouter la configuration et les fichiers pour la section noir

---
+++ src/web/desastre/recettes/noir/javascript/noir.js
@@ -0,0 +1,6 @@
+console.log('noir.js loaded');
+let contentDiv = document.querySelector('.content');
+
+let newDiv = document.createElement('div');
+newDiv.className = 'noir';
+contentDiv.appendChild(newDiv);
